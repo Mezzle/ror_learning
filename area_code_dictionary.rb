09c6f03fe84afb732a91152869e9403975c96267
Martin Meredith <martin.meredith@substrakthealth.com>
2020-08-06 15:53:14 +0100

    Area code dictionary

--- area_code_dictionary.rb
+++ area_code_dictionary.rb
@@ -1,27 +1,39 @@
 dial_book = {
-  "newyork" => "212",
-  "newbrunswick" => "732",
-  "edison" => "908",
-  "plainsboro" => "609",
-  "sanfrancisco" => "301",
-  "miami" => "305",
-  "paloalto" => "650",
-  "evanston" => "847",
-  "orlando" => "407",
-  "lancaster" => "717"
+  'newyork' => '212',
+  'newbrunswick' => '732',
+  'edison' => '908',
+  'plainsboro' => '609',
+  'sanfrancisco' => '301',
+  'miami' => '305',
+  'paloalto' => '650',
+  'evanston' => '847',
+  'orlando' => '407',
+  'lancaster' => '717'
 }
 
 # Get city names from the hash
 def get_city_names(somehash)
-  # Write code here
+  somehash.keys
 end
 
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-  # Write code here
+  somehash[key]
 end
 
 # Execution flow
 loop do
-  # Write your program execution code here
+  puts 'Do you want to lookup an area code based on a city name? (Y/N)'
+  answer = gets.chomp.downcase
+
+  exit unless answer == 'y'
+
+  get_city_names(dial_book).each do |city|
+    puts city.capitalize
+  end
+
+  puts 'Enter your selection'
+  city = gets.chomp.downcase
+
+  puts "The area code for #{city.capitalize} is #{get_area_code(dial_book, city)}"
 end
